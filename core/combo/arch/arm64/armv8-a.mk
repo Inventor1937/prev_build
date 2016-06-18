d52878565455aa18affac3244b60a4f5a0518b3f
Alex Naidis <alex.naidis@linux.com>
2016-06-18 19:38:12 +0200

    core: combo: armv8-a: specific kryo tuning
    * cortex-a57 tuning is currently the best for kryo
    * The "kryo" tuning in clang, seems to be real bad
    
    Change-Id: I5c3bdeb931b98e786a7173b34a3dd416f14f7f96
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,5 +1,15 @@
+ifneq (,$(filter cortex-a57.a53 cortex-a53.a57,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	arch_variant_cflags := -mcpu=cortex-a57.cortex-a53
+else
+ifneq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	arch_variant_cflags := -mcpu=cortex-a57
+else
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
 else
 	arch_variant_cflags :=
+endif
+
+endif
+
 endif
