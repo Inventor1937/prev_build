infected-lp/android_build

Make rtti default for gcc >=5.2
Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

core/binary.mk
@@ -918,9 +918,26 @@ installed_static_library_notice_file_targets := \
     $(foreach lib,$(my_static_libraries) $(my_whole_static_libraries), \
       NOTICE-$(if $(LOCAL_IS_HOST_MODULE),HOST,TARGET)-STATIC_LIBRARIES-$(lib))
 
-# Default is -fno-rtti.
-ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
-LOCAL_RTTI_FLAG := -fno-rtti
+ifeq (,$(filter 5.2% 6.0%,$(SM_AND_NAME)))
+
+  # Default is -fno-rtti.
+  ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
+    LOCAL_RTTI_FLAG := -fno-rtti
+  endif
+else
+  ifeq (,($(filter $(GCC_4_8_MODULES) $(GCC_4_9_MODULES),$(LOCAL_MODULE))))
+
+    # Default is -frtti.
+    ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
+      LOCAL_RTTI_FLAG := -frtti
+    endif
+  else
+
+    # Default is -fno-rtti.
+    ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
+      LOCAL_RTTI_FLAG := -fno-rtti
+    endif
+  endif
 endif
 
 ###########################################################

Inventor1937/prev_build

core/binary.mk
@@ -1391,9 +1391,26 @@ installed_static_library_notice_file_targets := \
     $(foreach lib,$(my_static_libraries) $(my_whole_static_libraries), \
       NOTICE-$(if $(LOCAL_IS_HOST_MODULE),HOST,TARGET)-STATIC_LIBRARIES-$(lib))
 
-# Default is -fno-rtti.
-ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
-LOCAL_RTTI_FLAG := -fno-rtti
+ifeq (,$(filter 5.2% 6.0%,$(TARGET_GCC_VERSION)))
+
+  # Default is -fno-rtti.
+  ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
+    LOCAL_RTTI_FLAG := -fno-rtti
+  endif
+else
+  ifeq (,($(filter $(GCC_4_8_MODULES) $(GCC_4_9_MODULES),$(LOCAL_MODULE))))
+
+    # Default is -frtti.
+    ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
+      LOCAL_RTTI_FLAG := -frtti
+    endif
+  else
+
+    # Default is -fno-rtti.
+    ifeq ($(strip $(LOCAL_RTTI_FLAG)),)
+      LOCAL_RTTI_FLAG := -fno-rtti
+    endif
+  endif
 endif
 
 ###########################################################